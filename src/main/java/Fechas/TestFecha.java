7afaa862069d3d84f95ae31b132fedbbeff98aa6
miel <m.ferrada09@ufromail.cl>
2023-09-27 09:51:02 -0300

    Probando si se pueden modificar atributos

--- src/main/java/Fechas/TestFecha.java
+++ src/main/java/Fechas/TestFecha.java
@@ -4,6 +4,15 @@ public class TestFecha {
     public static void main(String[] args) {
         TodaysDate hoy = new TodaysDate();
         hoy.printDateAndTime();
-    }
+        testearOcultamiento();
 
+    }
+    public static void testearOcultamiento(){
+        TodaysDate prueba = new TodaysDate();
+        prueba.time = "juan";
+        prueba.day = 2345;
+        prueba.year= 1998;
+        System.out.println(prueba.time + prueba.day + prueba.year);
+        prueba.printDateAndTime();
+    }
 }
